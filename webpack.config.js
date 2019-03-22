d368b8d6f5d5318b8ae3df2cf064b3107cb44f2b
rebecca-zhang <rebeccazhangnz@outlook.com>
2019-03-23 09:05:40 +1300

    set devServer contentbase to the html file dir

--- webpack.config.js
+++ webpack.config.js
@@ -8,27 +8,31 @@ const BUILD_DIR = path.resolve(__dirname, 'built')
 const APP_DIR = path.resolve(__dirname, 'src')
 
 module.exports = {
-//default is production mode, which the bundle file will be compressed
-//Please remember that setting NODE_ENV doesn't automatically set mode.
-//We can also add webpack-CLI in package.json and use "webpack --mode=production" 
-//in cmd to execute production mode/develompment mode.
+  //default is production mode, which the bundle file will be compressed
+  //Please remember that setting NODE_ENV doesn't automatically set mode.
+  //We can also add webpack-CLI in package.json and use "webpack --mode=production"
+  //in cmd to execute production mode/develompment mode.
   mode: 'development',
-// entry point indicates which module webpack should use to begin building out its internal dependency graph
-  entry: APP_DIR + '/index.js',
-// output indicates the config of the output bundle files
+  // entry point indicates which module webpack should use to begin building out its internal dependency graph
+  entry: [APP_DIR + '/index.js'],
+  // output indicates the config of the output bundle files
   output: {
-    path: BUILD_DIR,       //the dir of the output bundle file
+    path: BUILD_DIR, //the dir of the output bundle file
     filename: 'app.bundle.js', //the name of the outpub bundle file
-    publicPath: '/built/'   // default dir for other genterated files
+    publicPath: '/built/' // default dir for other genterated files
   },
-// module indicates the loaders and plugins that are being used
+  //devServer setup the dir for webpack-dev-server, this dir should point to the html file.
+  devServer: {
+    contentBase: APP_DIR,
+  },
+  // module indicates the loaders and plugins that are being used
   module: {
-//setting up loader:  Loaders can transform files from a different language (like TypeScript) to JavaScript or inline images as data URLs.
+    //setting up loader:  Loaders can transform files from a different language (like TypeScript) to JavaScript or inline images as data URLs.
     rules: [
       {
-        test: /\.js$/,  //apply babel loader to all js/jsx files
-        loader: 'babel-loader',//set babel as loader
-        exclude: /node_modules/ 
+        test: /\.js$/, //apply babel loader to all js/jsx files
+        loader: 'babel-loader', //set babel as loader
+        exclude: /node_modules/
       }
     ]
   },
